82cd1bb159454b6b74e68710b5b817aaac5444dd
seol <fullstack.eunseolgo14@gmail.com>
2025-10-15 20:55:23 +0900

    feat: 게임 맵 최단거리 문제 풀이 업로드

---
+++ 2025-10-15_shortest_game_map/MySubmit.java
@@ -0,0 +1,88 @@
+import java.util.*;
+
+class Block{
+    int col;
+    int row;
+    int time;
+    int value;
+    
+    Block(int col, int row, int time, int value){
+        this.col = col;
+        this.row = row;
+        this.time = time;
+        this.value = value;
+    }
+}
+
+class Solution {
+    
+    int mapSizeN;
+    int mapSizeM;
+        
+    public int solution(int[][] maps) {
+        
+        //맵 세로 길이.
+        mapSizeN = maps.length;
+        
+        //맵 가로 길이.
+        mapSizeM = maps[0].length;
+        
+        //큐 초기화, 방문 기록 초기화.
+        Queue<Block> steps = new LinkedList<>();
+        boolean visited[][] = new boolean[mapSizeN][mapSizeM];
+        
+        //상하좌우 이동 정의.
+        int[][] directions = {{1,0}, {-1,0}, {0,-1}, {0,1}};
+        
+        //0,0부터 탐색 시작.
+        steps.add(new Block(0, 0, 0, maps[0][0]));
+        visited[0][0] = true;
+
+        int maxCount = Integer.MAX_VALUE;
+
+        while(!steps.isEmpty()){
+
+            //[✏️ 틀린점 1]: 시간 초과 => 너무 큰 수까지 확인해야함.
+            if(maxCount < 0){
+                return -1;
+            }
+
+            Block curPos = steps.poll();
+            
+            if(isArrived(curPos)){
+                //[✏️ 틀린점 2]: 도착 까지의 시간을 의미하려면 여기서 +1되어야 함.
+                return curPos.time;
+            }
+            
+            for(int[] next : directions){
+                
+                int nCol = curPos.col + next[0];
+                int nRow = curPos.row + next[1];
+                int nTime = curPos.time + 1;
+                
+                if(isInRange(nCol, nRow)){
+                    if(!visited[nCol][nRow] && maps[nCol][nRow] == 1){
+                        //[✏️ 틀린점 3]: 방문 확인 누락, 큐에 무한대로 들어감.
+                        Block nextStep = new Block(nCol, nRow, nTime, maps[nCol][nRow]);
+                        steps.add(nextStep);
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    //보드 내 위치 여부 확인 유틸.
+    private boolean isInRange(int col, int row){
+        return (0 <= col && col < mapSizeN) && (0 <= row && row < mapSizeM);
+    }
+    
+    //도착 여부 확인 유틸.
+    private boolean isArrived(Block pos){
+        return pos.col == mapSizeN -1 && pos.row == mapSizeM -1;
+    }
+    
+}
